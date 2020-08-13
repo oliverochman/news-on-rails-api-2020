43c5b2e5e472fe436050b1aee8f44709ac5e6ae8
oliverochman <oliver.ochman@hotmail.com>
2020-08-13 09:26:33 +0200

    finish sad path

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+  before_action :authorize_user, only: [:create]
+
   def index
     if params['category']
       articles = Article.where(category: params['category'])
@@ -23,13 +26,20 @@ class Api::V1::ArticlesController < ApplicationController
     if article.persisted?
       render json: {message: "Articles successfully created"}
     else 
-
+      render_error_message(article.errors)
     end
    
   end
 
   private 
+
   def article_params
     params.permit(:title, :lead, :content, :category)
+  end
+
+  def authorize_user
+    unless current_user.role == 'journalist'
+      render json: {message: 'You are not authorized to access this action'}, status: 401
+    end
   end
 end
